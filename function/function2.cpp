a64bd2e7d9ef0158b92e950941f9b003f3f4989f
RITESH S.SONTAKKE <139070901+Ritesh1126@users.noreply.github.com>
2026-01-11 19:27:45 +0000

    funcitons

---
+++ function/function2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+void printHello()
+{
+    cout<< "Hello World"<<endl;
+}
+
+int main ()
+{
+    //function call or function invocation 
+    printHello ();
+
+    return 0;
+
+}
